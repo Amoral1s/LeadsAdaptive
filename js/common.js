712b96f47073b23002390e61ffde0b4f946cd46a
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-09-26 19:16:53 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -76,7 +76,48 @@ $(function () {
     $('.popup-callback2').removeClass('popup-callback2-active');
     $('.overlay').removeClass('overlay-active');
   });
-   
+  /*  
+  $('.about-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplayspeed: 3000,
+      speed: 500
+    }); */
+    $('.toggle-about').on('click', function () { 
+      $('.item_1').addClass('item-active');
+      $('.item_2').addClass('item-active');
+      $('.slider-buttons:nth-child(2)').addClass('about-active');
+     });
+    $('.toggle2').on('click', function () { 
+      $('.item_2').addClass('item-active2');
+      $('.item_3').addClass('item-active2');
+      $('.slider-buttons:nth-child(3)').addClass('about-active');
 
+     });
+    $('.toggle3').on('click', function () { 
+      $('.item_3').addClass('item-active3');
+      $('.item_4').addClass('item-active3');
+      $('.slider-buttons:nth-child(4)').addClass('about-active');
 
+     });
+    $('.toggle4').on('click', function () { 
+      $('.item_4').addClass('item-active4');
+      $('.item_5').addClass('item-active4');
+      $('.slider-buttons:nth-child(5)').addClass('about-active');
+
+     });
+    $('.reset').on('click', function () { 
+      $('.about-slider-item').removeClass('item-active');
+      $('.about-slider-item').removeClass('item-active3');
+      $('.about-slider-item').removeClass('item-active4');
+      $('.about-slider-item').removeClass('item-active1');
+      $('.about-slider-item').removeClass('item-active2');
+      $('.slider-buttons').removeClass('about-active');
+      $('.slider-buttons:first-child').addClass('about-active');
+
+     });
 });
